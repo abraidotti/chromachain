0dac6f7815018c68ffe8ac17bd7ff9fbf5ca3213
abraidotti <alessandro.braidotti@gmail.com>
2019-02-28 19:05:12 -0500

    add further boilerplate code

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io').listen(server);
  
 app.use(express.static(__dirname + '/public'));
  
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
  
-server.listen(8081, function () {
+server.listen(8081, () => {
   console.log(`Listening on ${server.address().port}`);
 });
